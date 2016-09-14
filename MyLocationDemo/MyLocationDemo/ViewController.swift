ba6f968b82dbcf51a928b138d86d0ba624710cdf
tannerwh <tannerwh@gmail.com>
2016-09-14 10:07:03 -0500

    Updated MyLocationDemo

--- MyLocationDemo/MyLocationDemo/ViewController.swift
+++ MyLocationDemo/MyLocationDemo/ViewController.swift
@@ -14,28 +14,28 @@ class ViewController : UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
-        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        let initialLocation = CLLocation(latitude: 38.865211, longitude: -94.633979)
         centerMapOnLocation(initialLocation)
-        
     }
     
-    let regionRadius: CLLocationDistance = 1000
-    func centerMapOnLocation(location: CLLocation) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    let regionRadius: CLLocationDistance = 2000
+    
+    func centerMapOnLocation(_ location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
                                                                   regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
         
     }
-    
-    
+}
     //@IBOutlet weak var map: MKMapView!
 //    var locationManager: CLLocationManager!
 //    
@@ -63,4 +63,3 @@ class ViewController : UIViewController, CLLocationManagerDelegate {
 //        
 //        self.map.setRegion(region, animated: true)
 //    }
-}
