7891cd2bd3bd180d0caf6d29274713c5e154e1b6
tannerwh <tannerwh@gmail.com>
2016-10-05 10:45:32 -0500

    Update nodechat

--- NodeChat/server.js
+++ NodeChat/server.js
@@ -10,3 +10,9 @@ var server = http.listen(3000, function() {
 	console.log('hosting from ' + __dirname);
 	console.log('server listening on http://localhost:3000/');
 });
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.emit('welcome', { text : 'OH HAI! U R CONNECTED ' });
+});
