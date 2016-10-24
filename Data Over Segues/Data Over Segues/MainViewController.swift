f89cd032beb94ff924223b0729a2c1aa887eadfe
tannerwh <tannerwh@gmail.com>
2016-10-24 14:05:32 -0500

    Created segue

---
+++ Data Over Segues/Data Over Segues/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Data Over Segues
+//
+//  Created by Tanner Helton on 10/24/16.
+//  Copyright © 2016 Tanner Helton. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var txtMessage: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let reciever = segue.destination as! SecondViewController
+        reciever.msg = txtMessage.text
+    }
+}
+
---
+++ Data Over Segues/Data Over Segues/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Data Over Segues
+//
+//  Created by Tanner Helton on 10/24/16.
+//  Copyright © 2016 Tanner Helton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var msg: String?
+    
+    @IBOutlet weak var lblMessage: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lblMessage.text = msg
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let reciever = segue.destination as! ThirdViewController
+        reciever.msg = lblMessage.text
+    }
+}
