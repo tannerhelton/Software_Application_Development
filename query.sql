4ae9dd71f00839385bc1bb82be834b62c3e20603
tannerwh <tannerwh@gmail.com>
2016-11-17 14:05:12 -0600

    Updated query.sql

--- query.sql
+++ query.sql
@@ -21,3 +21,36 @@ AND c.last_name = "Tubbs";
 
 --4) How many times has Duane Tubbs rented?
 
+SELECT c.first_name, c.last_name, COUNT(a.rental_id) as num_rentals
+FROM customer c
+JOIN rental a
+ON a.customer_id = c.customer_id
+WHERE c.first_name = "Duane"
+AND c.last_name = "Tubbs";
+
+--5) How much money did Duane Tubbs spend on rentals?
+
+SELECT c.first_name, c.last_name, 
+   SUM(p.amount) AS "total_payments"
+FROM customer c
+JOIN payment p ON p.customer_id = c.customer_id
+WHERE c.first_name = "Duane"
+AND c.last_name = "Tubbs"
+GROUP BY last_name;
+
+--) 34 France
+
+SELECT country_id, country
+FROM `country`
+WHERE country = "France";
+
+--6) How much in total did each person whose last name starts with "T" spend on rentals?
+
+SELECT c.first_name, c.last_name, 
+   SUM(p.amount) AS "total_payments"
+FROM customer c
+JOIN payment p ON p.customer_id = c.customer_id
+WHERE c.last_name LIKE "T%"
+GROUP BY last_name;
+
+--7) 
